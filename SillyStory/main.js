21c8d61e8de0a8836fff24146958adec7c57ed03
CEWDLAB <cewdlab@campus.minneapolis.edu>
2018-11-29 21:08:31 -0600

    adding silly story

---
+++ SillyStory/main.js
@@ -0,0 +1,67 @@
+console.log("check");
+
+
+
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  return array[Math.floor(Math.random()*array.length)];
+}
+
+var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+var insertX = [
+"Willy the Goblin",
+"Your Mom",
+"Father Christmas"
+];
+
+var insertY = [
+"the soup kitchen",
+"Disneyland",
+"the White House"
+];
+
+var insertZ = [
+"spontaneously combusted",
+"melted into a puddle on the sidewalk",
+"turned into a multi-colored beetle and crawled away"
+];
+
+randomize.addEventListener('click', result);
+console.log(randomize);
+
+function result() {
+
+console.log("hello from result function");
+  var newStory = storyText;
+
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(":insertx:", xItem);
+  newStory = newStory.replace(":inserty:", yItem);
+  newStory = newStory.replace(":insertz:", zItem);
+  newStory = newStory.replace(":insertx:", xItem);
+
+  if(customName.value !== '') {
+    var name = customName.value;
+    newStory = newStory.replace("Bob", name);
+  }
+
+  if(document.getElementById("uk").checked) {
+    var weight = Math.round(300/14) + " stone";
+    var temperature = Math.round((94-32)*5/9) + " Centigrade";
+
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 fahrenheit", temperature);
+    newStory = newStory.replace("multi-colored", "multi-coloured");
+
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
